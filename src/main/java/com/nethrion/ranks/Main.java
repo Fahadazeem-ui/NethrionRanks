7a34f654a4001434fb244975f5222143af1ec79d
Fahadazeem-ui <ziddibusiness@gmail.com>
2026-08-17 08:02:57 +0500

    Add Rank and Skills module

--- src/main/java/com/nethrion/ranks/Main.java
+++ src/main/java/com/nethrion/ranks/Main.java
@@ -1,15 +1,24 @@
 package com.nethrion.ranks;
 
+import com.nethrion.ranks.commands.RankCommand;
+import com.nethrion.ranks.commands.SkillsCommand;
+import com.nethrion.ranks.managers.RankManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
+
+    private RankManager rankManager;
 
     @Override
     public void onEnable() {
         getLogger().info("NethrionRanks is starting up...");
 
-        // Managers, commands, aur listeners yahan register honge
-        // jaise jaise hum modules banate jayenge
+        // Managers create karna
+        this.rankManager = new RankManager(this);
+
+        // Commands register karna
+        getCommand("rank").setExecutor(new RankCommand(rankManager));
+        getCommand("skills").setExecutor(new SkillsCommand(rankManager));
 
         getLogger().info("NethrionRanks enabled successfully!");
     }
